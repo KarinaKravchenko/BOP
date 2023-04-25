2ec7eff3c43958fbc81ff2680649c602d839b364
KarinaK <ms.karina.kravchenko.96@mail.ru>
2023-04-25 17:45:53 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,5 +1,51 @@
-//
-// Created by mskar on 25.04.2023.
-//
-
 #include "vector.h"
+
+void errorMemory() {
+    fprintf(stderr, "bad alloc");
+    exit(1);
+}
+
+vector createVector(size_t n) {
+    int* a = (int*) malloc(sizeof(int) * n);
+
+    if (a == NULL) {
+        errorMemory();
+    }
+
+    vector v = {n ? a : NULL, 0, n};
+
+    return v;
+}
+
+void reserve(vector *v, size_t newCapacity) {
+    if (newCapacity == 0) {
+        v->data = NULL;
+        v->size = 0;
+        v->capacity = 0;
+    }
+    else if (newCapacity > v->capacity) {
+        v->data = (int*) realloc(v->data, sizeof(v->data[0]) * newCapacity);
+        v->capacity = newCapacity;
+        if (v->data == NULL) {
+            errorMemory();
+        }
+    }
+    else if (newCapacity < v->capacity) {
+        if (newCapacity < v->size) {
+            v->size = newCapacity;
+        }
+        v->capacity = newCapacity;
+    }
+}
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    v->data = (int*) realloc(v->data, sizeof(v->data[0]) * v->size);
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+}
--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,8 +1,34 @@
-//
-// Created by mskar on 25.04.2023.
-//
-
 #ifndef BPROGRAMMING_VECTOR_H
 #define BPROGRAMMING_VECTOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef struct vector {
+    int *data;       // указатель на элементы вектора
+    size_t size;     // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+// возвращает структуру вектор длинны n
+vector createVector(size_t n);
+
+// изменяет количество выделенной памяти на newCapacity
+// структуре вектор v
+void reserve(vector *v, size_t newCapacity);
+
+// удаляет все элементы из структуры вектор v, но не
+// освобождает выделенную память
+void clear(vector *v);
+
+// освобождает неиспользуюмую память, выделенную структуре
+// вектор v
+void shrinkToFit(vector *v);
+
+// освобождает память, выделенную структуре вектор v
+void deleteVector(vector *v);
 
 #endif //BPROGRAMMING_VECTOR_H
